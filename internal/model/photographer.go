ebc02676632b276307b823dc12328431d8f49a0b
Jeerasak Kajonrattanavanit <mazuroze@gmail.com>
2025-02-08 16:35:25 +0700

    refactor: remove ID & add one2many relation

--- internal/model/photographer.go
+++ internal/model/photographer.go
@@ -4,11 +4,11 @@ import "gorm.io/gorm"
 
 type Photographer struct {
 	gorm.Model
-	UserID           uint   `gorm:uniqueIndex;not null`
+	UserID           uint   `gorm:"uniqueIndex;not null"`
 	User             User   `gorm:"foreignKey:UserID"`
-	SSN              string `gorm:"size:10+-;not null"`
+	SSN              string `gorm:"size:10;not null"`
 	IsVerified       bool   `gorm:"not null;default:false"`
 	ActiveStatus     bool   `gorm:"not null;default:false"`
 	IDCardPictureURL string
-	Gallery          []Gallery `gorm:"foreignKey:GalleryID"`
+	Gallery          []Gallery `gorm:"foreignKey:PhotographerID"`
 }
