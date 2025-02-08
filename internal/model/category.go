67fb9cf1200c69dafedaed6ec71ccc33fec6e394
Jeerasak Kajonrattanavanit <mazuroze@gmail.com>
2025-02-08 20:48:16 +0700

    feat: more relations

--- internal/model/category.go
+++ internal/model/category.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type Category struct {
 	gorm.Model
-	Name        string `gorm:"not null"`
-	Description string `gorm:"not null"`
+	Name        string    `gorm:"not null"`
+	Description string    `gorm:"not null"`
+	Galleries   []Gallery `gorm:"many2many:Galleries_Categories"`
 }
