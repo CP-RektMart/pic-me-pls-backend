82f41a33b0faa19926f6c74d57194fadc55b3c61
Thiraput01 <Thiraputball@gmail.com>
2025-02-08 11:09:37 +0700

    refactor: remove fields that's already in gorm.model

--- internal/model/media.go
+++ internal/model/media.go
@@ -4,7 +4,6 @@ import "gorm.io/gorm"
 
 type Media struct {
 	gorm.Model
-	ID         uint    `gorm:"primaryKey"`
 	GalleryID  uint    `gorm:"not null"`
 	Gallery    Gallery `gorm:"foreignKey:GalleryID"`
 	PictureURL string  `gorm:"not null"`
