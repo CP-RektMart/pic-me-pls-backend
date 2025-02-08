bff1044c4cbe464f9bf270b9fa3db3323e76a2f9
Thiraput01 <Thiraputball@gmail.com>
2025-02-08 13:05:42 +0700

    refactor: add gallery field and index column

--- internal/model/gallery.go
+++ internal/model/gallery.go
@@ -8,5 +8,6 @@ type Gallery struct {
 	Photographer   Photographer `gorm:"foreignKey:PhotographerID"`
 	Name           string       `gorm:"not null"`
 	Description    string
-	Price          float64 `gorm:"not null"`
+	Price          float64   `gorm:"not null"`
+	Gallery        []Gallery `gorm:"foreignKey:GalleryID"`
 }
