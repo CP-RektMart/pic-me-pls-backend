316feebc4baf8a3b6ce3ba88294f4b2abcf17b65
Thiraput01 <Thiraputball@gmail.com>
2025-02-08 12:45:52 +0700

    refactor: (model/quotation.go)remove createAt, updateAt

--- internal/model/quotation.go
+++ internal/model/quotation.go
@@ -1,8 +1,6 @@
 package model
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
@@ -15,8 +13,6 @@ type Quotation struct {
 	Customer       User         `gorm:"foreignKey:CustomerID"`
 	PhotographerID uint         `gorm:"not null"`
 	Photographer   Photographer `gorm:"foreignKey:PhotographerID"`
-	CreatedAt      time.Time    `gorm:"not null"`
-	UpdatedAt      time.Time    `gorm:"not null"`
 	Status         string       `gorm:"not null"` // pending, confirm, cancelled, paid
 	Price          float64      `gorm:"not null"`
 	Description    string       `gorm:"not null"`
