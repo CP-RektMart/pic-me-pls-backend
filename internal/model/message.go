10f5546c6b693af271bf31643107b6ac0afc8ddf
Thiraput01 <Thiraputball@gmail.com>
2025-02-08 12:44:59 +0700

    refactor: (model/message.gp)remove createAt

--- internal/model/message.go
+++ internal/model/message.go
@@ -1,18 +1,15 @@
 package model
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type Message struct {
 	gorm.Model
-	ID         uint      `gorm:"primaryKey"`
-	SenderID   uint      `gorm:"not null"`
-	Sender     User      `gorm:"foreignKey:SenderID"`
-	ReceiverID uint      `gorm:"not null"`
-	Receiver   User      `gorm:"foreignKey:ReceiverID"`
-	Content    string    `gorm:"not null"`
-	CreatedAt  time.Time `gorm:"not null"`
+	ID         uint   `gorm:"primaryKey"`
+	SenderID   uint   `gorm:"not null"`
+	Sender     User   `gorm:"foreignKey:SenderID"`
+	ReceiverID uint   `gorm:"not null"`
+	Receiver   User   `gorm:"foreignKey:ReceiverID"`
+	Content    string `gorm:"not null"`
 }
