d2823ad3a41dc0d4b32abeedf5a510d8e1f72b25
teegoood <teegooodgood@gmail.com>
2025-02-08 11:14:29 +0700

    feat: add error handler in request logger middleware

--- pkg/requestlogger/requestlogger.go
+++ pkg/requestlogger/requestlogger.go
@@ -1,8 +1,11 @@
 package requestlogger
 
 import (
+	"errors"
 	"log/slog"
 
+	"github.com/CP-RektMart/pic-me-pls-backend/internal/dto"
+	"github.com/CP-RektMart/pic-me-pls-backend/pkg/apperror"
 	"github.com/CP-RektMart/pic-me-pls-backend/pkg/logger"
 	"github.com/gofiber/fiber/v2"
 )
@@ -10,7 +13,34 @@ import (
 func New() func(ctx *fiber.Ctx) error {
 	return func(ctx *fiber.Ctx) error {
 		requestId := ctx.GetRespHeader("X-Request-Id")
-		logger.InfoContext(ctx.UserContext(), "request received", slog.String("request_id", requestId), slog.String("method", ctx.Method()), slog.String("path", ctx.Path()))
-		return ctx.Next()
+		err := ctx.Next()
+		if err != nil {
+			return handleError(ctx, err)
+		}
+
+		logger.InfoContext(ctx.UserContext(), "request received", slog.String("request_id", requestId), slog.String("method", ctx.Method()), slog.String("path", ctx.Path()), slog.Int("status", ctx.Response().StatusCode()))
+		return nil
 	}
+}
+
+func handleError(c *fiber.Ctx, err error) error {
+	var fiberError *fiber.Error
+	if errors.As(err, &fiberError) {
+		c.Set(fiber.HeaderContentType, fiber.MIMETextPlainCharsetUTF8)
+		return c.Status(fiberError.Code).SendString(fiberError.Message)
+	}
+
+	status := fiber.StatusInternalServerError
+	message := "internal server error"
+
+	var appError *apperror.AppError
+	if errors.As(err, &appError) {
+		status = appError.Code
+		message = appError.Message
+	}
+
+	logger.ErrorContext(c.UserContext(), "Request Error", slog.Int("status", status), slog.Any("error", err))
+	return c.Status(status).JSON(dto.HttpResponse{
+		Error: message,
+	})
 }
