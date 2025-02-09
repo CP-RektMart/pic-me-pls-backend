3b8b9125eba2dfc20be402e1621f034f74299e7d
K.Chanatipz <chanatipzaza2003@gmail.com>
2025-02-09 16:31:47 +0700

    feat: get me logic

--- internal/server/profile.go
+++ internal/server/profile.go
@@ -1,7 +1,19 @@
 package server
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/CP-RektMart/pic-me-pls-backend/internal/model"
+	"github.com/CP-RektMart/pic-me-pls-backend/pkg/apperror"
+	"github.com/gofiber/fiber/v2"
+)
 
 func (s *Server) handleGetMe(c *fiber.Ctx) error {
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "นี่คือเสียงจากเด็กวัด"})
+	userDto, ok := c.Locals("user").(*model.UserDto)
+
+	if !ok {
+		return apperror.BadRequest("no user profile found in context", nil)
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"profile": userDto,
+	})
 }
