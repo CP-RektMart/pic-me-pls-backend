a4b275fc406cbe4abea6a36994e2733bfa6b9599
tin2003tin <152775124+tin2003tin@users.noreply.github.com>
2025-02-11 22:57:04 +0700

    remove ssn, already have citizen card

--- internal/model/photographer.go
+++ internal/model/photographer.go
@@ -6,7 +6,7 @@ type Photographer struct {
 	gorm.Model
 	UserID       uint      `gorm:"uniqueIndex;not null"`
 	User         User      `gorm:"foreignKey:UserID"`
-	SSN          string    `gorm:"size:13;not null"`
+
 	IsVerified   bool      `gorm:"not null;default:false"`
 	ActiveStatus bool      `gorm:"not null;default:false"`
 	Galleries    []Gallery `gorm:"foreignKey:PhotographerID"`
