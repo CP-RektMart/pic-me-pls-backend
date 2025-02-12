3a47bb0ea312f34dedb7ddd929ee59b4426faf10
teegoood <teegooodgood@gmail.com>
2025-02-12 20:49:54 +0700

    chore: camelcase dto

--- internal/dto/user.go
+++ internal/dto/user.go
@@ -5,26 +5,26 @@ import "github.com/CP-RektMart/pic-me-pls-backend/internal/model"
 type UserUpdateRequest struct {
 	Name        string `form:"name"`
 	Email       string `form:"email"`
-	PhoneNumber string `form:"phone_number"`
+	PhoneNumber string `form:"phoneNumber"`
 	Facebook    string `form:"facebook"`
 	Instagram   string `form:"instagram"`
 	Bank        string `form:"bank"`
-	AccountNo   string `form:"account_no"`
-	BankBranch  string `form:"bank_branch"`
+	AccountNo   string `form:"accountNo"`
+	BankBranch  string `form:"bankBranch"`
 }
 
 type UserResponse struct {
 	ID                uint   `json:"id"`
 	Name              string `json:"name"`
 	Email             string `json:"email"`
-	PhoneNumber       string `json:"phone_number"`
-	ProfilePictureURL string `json:"profile_picture_url"`
+	PhoneNumber       string `json:"phoneNumber"`
+	ProfilePictureURL string `json:"profilePicture_url"`
 	Role              string `json:"role"`
 	Facebook          string `json:"facebook,omitempty"`
 	Instagram         string `json:"instagram,omitempty"`
 	Bank              string `json:"bank,omitempty"`
-	AccountNo         string `json:"account_no,omitempty"`
-	BankBranch        string `json:"bank_branch,omitempty"`
+	AccountNo         string `json:"accountNo,omitempty"`
+	BankBranch        string `json:"bankBranch,omitempty"`
 }
 
 func ToUserResponse(user model.User) UserResponse {
