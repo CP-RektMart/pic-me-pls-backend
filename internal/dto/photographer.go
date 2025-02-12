4ce2cae4d55ec2da06c7783bba042fb9d545367d
tintin <tinsiriwid@gmail.com>
2025-02-12 13:00:04 +0700

    Revert "fix: dto required field"
    This reverts commit a3bbe97de49230af49411a30fbf58e88b91b43b5.

--- internal/dto/photographer.go
+++ internal/dto/photographer.go
@@ -3,10 +3,17 @@ package dto
 import "time"
 
 type CitizenCardRequest struct {
+<<<<<<< HEAD
 	CitizenID  string    `json:"citizenId" validate:"required"`
 	LaserID    string    `json:"laserId" validate:"required"`
 	Picture    string    `json:"picture" validate:"required"`
 	ExpireDate time.Time `json:"expireDate" validate:"required"`
+=======
+	CitizenID  string                `from:"citizenId"`
+	LaserID    string                `from:"laserId"`
+	Picture    *multipart.FileHeader `from:"picture"`
+	ExpireDate time.Time             `from:"expireDate"`
+>>>>>>> parent of a3bbe97 (fix: dto required field)
 }
 
 type CitizenCardResponse struct {
