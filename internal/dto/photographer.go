2248ab846ea0fe2c0baa54dcd4b9ed71b350677c
Jeerasak Kajonrattanavanit <mazuroze@gmail.com>
2025-02-14 13:13:24 +0700

    add photographer dto

--- internal/dto/photographer.go
+++ internal/dto/photographer.go
@@ -1,6 +1,10 @@
 package dto
 
-import "time"
+import (
+	"time"
+
+	"github.com/CP-RektMart/pic-me-pls-backend/internal/model"
+)
 
 // Form data
 type CitizenCardRequest struct {
@@ -14,4 +18,19 @@ type CitizenCardResponse struct {
 	LaserID    string    `json:"laserId"`
 	Picture    string    `json:"picture"`
 	ExpireDate time.Time `json:"expireDate"`
+}
+
+type PhotographerResponse struct {
+	ID           uint   `json:"id"`
+	Name         string `json:"name"`
+	IsVerified   bool   `json:"is_verified"`
+	ActiveStatus bool   `json:"active_status"`
+}
+
+func ToPhotographerResponse(photographer model.Photographer) PhotographerResponse {
+	return PhotographerResponse{
+		ID:           photographer.ID,
+		IsVerified:   photographer.IsVerified,
+		ActiveStatus: photographer.ActiveStatus,
+	}
 }
