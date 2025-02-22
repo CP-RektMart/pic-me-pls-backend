103e141f3f49ddd1f917bcb14cf5ed697a6e56be
Thiraput01 <Thiraputball@gmail.com>
2025-02-22 23:03:01 +0700

    refactor: from []T to T

--- internal/dto/pagination.go
+++ internal/dto/pagination.go
@@ -9,5 +9,5 @@ type PaginationResponse[T any] struct {
 	Page      int `json:"page"`
 	PageSize  int `json:"pageSize"`
 	TotalPage int `json:"totalPage"`
-	Data      []T `json:"data"`
+	Data      T   `json:"data"`
 }
