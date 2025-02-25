7308452ccf7cb6f826d7fa73a4cb1a753fbe3cf2
boomchanotai <boom9387@hotmail.com>
2025-02-25 15:46:25 +0700

    fix: delete and clean url

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -2,7 +2,11 @@ package storage
 
 import (
 	"context"
+	"fmt"
 	"io"
+	"net/url"
+	"path"
+	"strings"
 
 	"github.com/cockroachdb/errors"
 	storage_go "github.com/supabase-community/storage-go"
@@ -17,7 +21,7 @@ type Config struct {
 
 type Client struct {
 	Client *storage_go.Client
-	Bucket string
+	config Config
 }
 
 func New(ctx context.Context, config Config) (*Client, error) {
@@ -28,23 +32,28 @@ func New(ctx context.Context, config Config) (*Client, error) {
 
 	return &Client{
 		Client: client.Storage,
-		Bucket: config.Bucket,
+		config: config,
 	}, nil
 }
 
 func (c *Client) UploadFile(ctx context.Context, path string, contentType string, data io.Reader, overwrite bool) (string, error) {
-	if _, err := c.Client.UploadFile(c.Bucket, path, data, storage_go.FileOptions{
+	if _, err := c.Client.UploadFile(c.config.Bucket, path, data, storage_go.FileOptions{
 		ContentType: &contentType,
 		Upsert:      &overwrite,
 	}); err != nil {
 		return "", errors.Wrap(err, "failed to upload file")
 	}
 
-	return c.Client.GetPublicUrl(c.Bucket, path).SignedURL, nil
+	fileURL, err := c.CleanURL(c.Client.GetPublicUrl(c.config.Bucket, path).SignedURL)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to clean url")
+	}
+
+	return fileURL, nil
 }
 
 func (c *Client) MoveFile(ctx context.Context, source string, destination string) error {
-	if _, err := c.Client.MoveFile(c.Bucket, source, destination); err != nil {
+	if _, err := c.Client.MoveFile(c.config.Bucket, source, destination); err != nil {
 		return errors.Wrap(err, "failed to move file")
 	}
 
@@ -59,9 +68,44 @@ func (c *Client) DeleteFile(ctx context.Context, path string) error {
 }
 
 func (c *Client) DeleteFiles(ctx context.Context, path []string) error {
-	if _, err := c.Client.RemoveFile(c.Bucket, path); err != nil {
+	relativePaths := make([]string, 0, len(path))
+	for _, p := range path {
+		relativePath, err := c.RelativePath(p)
+		if err != nil {
+			return errors.Wrap(err, "failed to get relative path")
+		}
+		relativePaths = append(relativePaths, relativePath)
+	}
+
+	if _, err := c.Client.RemoveFile(c.config.Bucket, relativePaths); err != nil {
 		return errors.Wrap(err, "failed to delete file")
 	}
 
 	return nil
+}
+
+func (c *Client) CleanURL(rawURL string) (string, error) {
+	parsedURL, err := url.Parse(rawURL)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to parse url")
+	}
+
+	// Clean up the path using path.Clean
+	parsedURL.Path = path.Clean(parsedURL.Path)
+
+	return parsedURL.String(), nil
+}
+
+func (c *Client) RelativePath(path string) (string, error) {
+	bucketURL, err := url.Parse(c.config.Url)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to parse bucket url")
+	}
+
+	cleanedUrl, err := c.CleanURL(path)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to clean url")
+	}
+
+	return strings.Replace(cleanedUrl, fmt.Sprintf("https://%s/storage/v1/object/public/%s/", bucketURL.Host, c.config.Bucket), "", 1), nil
 }
