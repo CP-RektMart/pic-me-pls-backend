e497cb4c5b77297ca145deeb286249b4cb15dd85
K.Chanatipz <chanatipzaza2003@gmail.com>
2025-03-11 17:23:33 +0700

    refactor: improve code

--- internal/dto/photographer.go
+++ internal/dto/photographer.go
@@ -56,14 +56,18 @@ func ToPhotographerResponse(photographer model.Photographer) PhotographerRespons
 	}
 }
 
+func ToSmallPackageResponse(pkg model.Package) SmallPackageResponse {
+	return SmallPackageResponse{
+		ID:          pkg.ID,
+		Name:        pkg.Name,
+		Description: pkg.Description,
+	}
+}
+
 func ToSmallPackageResponses(packages []model.Package) []SmallPackageResponse {
 	var responses []SmallPackageResponse
-	for _, p := range packages {
-		responses = append(responses, SmallPackageResponse{
-			ID:          p.ID,
-			Name:        p.Name,
-			Description: p.Description,
-		})
+	for _, pkg := range packages {
+		responses = append(responses, ToSmallPackageResponse(pkg))
 	}
 	return responses
 }
