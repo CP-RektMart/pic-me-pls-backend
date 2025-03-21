61f9427e671e1c91db3205c63048476d719fffcb
teegoood <teegooodgood@gmail.com>
2025-03-21 14:38:11 +0700

    refactor: chatsystem

--- internal/services/chat/chat_system.go
+++ internal/services/chat/chat_system.go
@@ -1,15 +1,25 @@
 package chat
 
 import (
+	"encoding/json"
+	"fmt"
+	"log/slog"
 	"sync"
 
 	"github.com/CP-RektMart/pic-me-pls-backend/internal/database"
-	"github.com/CP-RektMart/pic-me-pls-backend/internal/model"
-	"github.com/cockroachdb/errors"
+	"github.com/CP-RektMart/pic-me-pls-backend/internal/dto"
+	"github.com/CP-RektMart/pic-me-pls-backend/pkg/logger"
+)
+
+type EventType string
+
+const (
+	EventError   EventType = "ERROR"
+	EventMessage EventType = "MESSAGE"
 )
 
 type ChatSystem struct {
-	clients map[uint]chan model.Message
+	clients map[uint]chan string
 	store   *database.Store
 }
 
@@ -21,7 +31,7 @@ var (
 func NewChatSystem(store *database.Store) *ChatSystem {
 	once.Do(func() {
 		instance = &ChatSystem{
-			clients: make(map[uint]chan model.Message),
+			clients: make(map[uint]chan string),
 			store:   store,
 		}
 	})
@@ -29,8 +39,8 @@ func NewChatSystem(store *database.Store) *ChatSystem {
 	return instance
 }
 
-func (c *ChatSystem) Register(userID uint) chan model.Message {
-	channel := make(chan model.Message)
+func (c *ChatSystem) Register(userID uint) chan string {
+	channel := make(chan string)
 	c.clients[userID] = channel
 	return channel
 }
@@ -41,18 +51,42 @@ func (c *ChatSystem) Logout(userID uint) {
 	}
 }
 
-func (c *ChatSystem) SendMessage(message model.Message) error {
-	receiverID := message.ReceiverID
-
-	if err := c.store.DB.Create(&message).Error; err != nil {
-		return errors.Wrap(err, "failed inserting message to database")
+func (c *ChatSystem) SendMessage(senderID uint, msg string) {
+	var msgReq dto.RealTimeMessageRequest
+	if err := json.Unmarshal([]byte(msg), &msgReq); err != nil {
+		logger.Error("Failed Unmarshal json", slog.Any("error", err))
+		c.sendMessage(EventError, senderID, "invalid message")
+		return
 	}
 
+	msgModel := dto.ToMessageModel(senderID, msgReq)
+	if msgModel.ReceiverID == senderID {
+		logger.Error("cannot send message to yourself")
+		c.sendMessage(EventError, senderID, "cannot send message to yourself")
+		return
+	}
+
+	if err := c.store.DB.Create(&msgModel).Error; err != nil {
+		logger.Error("failed inserting message to database", slog.Any("error", err))
+		c.sendMessage(EventError, senderID, "internal error")
+		return
+	}
+
+	json, err := json.Marshal(dto.ToRealTimeMessageResponse(msgModel))
+	if err != nil {
+		logger.Error("failed Marshal realtime message response to json", slog.Any("error", err))
+		c.sendMessage(EventError, senderID, "internal error")
+		return
+	}
+
+	c.sendMessage(EventMessage, msgModel.ReceiverID, string(json))
+}
+
+func (c *ChatSystem) sendMessage(event EventType, receiverID uint, msg string) {
+	msg = fmt.Sprintf("%s %s", event, msg)
 	if c.IsUserExist(receiverID) {
-		c.clients[receiverID] <- message
+		c.clients[receiverID] <- msg
 	}
-
-	return nil
 }
 
 func (c *ChatSystem) IsUserExist(userID uint) bool {
--- internal/services/chat/realtime_chat.go
+++ internal/services/chat/realtime_chat.go
@@ -1,22 +1,20 @@
 package chat
 
 import (
-	"encoding/json"
 	"sync"
 
-	"github.com/CP-RektMart/pic-me-pls-backend/internal/dto"
 	"github.com/CP-RektMart/pic-me-pls-backend/internal/jwt"
-	"github.com/CP-RektMart/pic-me-pls-backend/internal/model"
 	"github.com/CP-RektMart/pic-me-pls-backend/pkg/logger"
-	"github.com/cockroachdb/errors"
 	"github.com/gofiber/contrib/websocket"
 )
 
 func (h *Handler) HandleRealTimeChat(c *websocket.Conn) {
+	defer c.Close()
+
 	jwtEntity, ok := c.Locals(jwtEntityKey).(jwt.JWTentity)
 	if !ok {
 		logger.Error("failed receive userID from jwtEntity")
-		c.Close()
+		return
 	}
 
 	ch := h.chatSystem.Register(jwtEntity.ID)
@@ -25,12 +23,10 @@ func (h *Handler) HandleRealTimeChat(c *websocket.Conn) {
 	wg.Add(2)
 
 	go h.receiveRealtimeMessage(&wg, c, jwtEntity.ID)
-	go h.sendRealtimeMessage(&wg, ch, c)
+	go h.sendRealtimeMessage(&wg, c, ch)
 
 	wg.Wait()
-
 	h.chatSystem.Logout(jwtEntity.ID)
-	c.Close()
 }
 
 func (h *Handler) receiveRealtimeMessage(wg *sync.WaitGroup, c *websocket.Conn, userID uint) {
@@ -45,78 +41,21 @@ func (h *Handler) receiveRealtimeMessage(wg *sync.WaitGroup, c *websocket.Conn, 
 		}
 
 		if mt == websocket.TextMessage {
-			var message dto.RealTimeMessageRequest
-			if err := json.Unmarshal(msg, &message); err != nil {
-				logger.Error("invalid message", err)
-				if err := h.sendErrorMessage(c, "invalid message"); err != nil {
-					logger.Error("failed sending error", err)
-				}
-				continue
-			}
-			if err := h.validate.Struct(message); err != nil {
-				logger.Error("invalid message", err)
-				if err := h.sendErrorMessage(c, "invalid message"); err != nil {
-					logger.Error("failed sending error", err)
-				}
-				continue
-			}
-			if userID == message.ReceiverID {
-				logger.Error("receiverID is same as userID")
-				if err := h.sendErrorMessage(c, "receiverID is same as userID"); err != nil {
-					logger.Error("failed sending error", err)
-				}
-				continue
-			}
-
-			if err := h.chatSystem.SendMessage(dto.ToMessageModel(userID, message)); err != nil {
-				logger.Error("failed sending message", err)
-				if err := h.sendErrorMessage(c, "internal error"); err != nil {
-					logger.Error("failed sending error", err)
-				}
-				continue
-			}
+			h.chatSystem.SendMessage(userID, string(msg))
 		}
 	}
 }
 
-func (h *Handler) sendRealtimeMessage(wg *sync.WaitGroup, ch chan model.Message, c *websocket.Conn) {
+func (h *Handler) sendRealtimeMessage(wg *sync.WaitGroup, c *websocket.Conn, ch chan string) {
 	defer wg.Done()
 
 	for {
 		msg := <-ch
 
-		json, err := json.Marshal(dto.ToRealTimeMessageResponse(msg))
-		if err != nil {
-			logger.Error("failed convert message to json")
-			if err := h.sendErrorMessage(c, "internal error"); err != nil {
-				logger.Error("failed sending error", err)
-			}
-		}
-
-		if err := c.WriteMessage(websocket.TextMessage, json); err != nil {
+		if err := c.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
 			logger.Error("failed sending message", err)
 			logger.Info("closing connection...")
 			break
 		}
 	}
-}
-
-func (h *Handler) sendErrorMessage(c *websocket.Conn, msg string) error {
-	response := dto.ToRealTimeMessageResponse(
-		model.Message{
-			Type:    model.MessageTypeError,
-			Content: msg,
-		},
-	)
-
-	json, err := json.Marshal(&response)
-	if err != nil {
-		return errors.Wrap(err, "failed marshal json")
-	}
-
-	if err := c.WriteMessage(websocket.TextMessage, json); err != nil {
-		return errors.Wrap(err, "failed sending error message")
-	}
-
-	return nil
 }
