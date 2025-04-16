7e94bc769d797d390f0e768e0fa0f0cf376e1e12
TeeGoood <teegooodgood@gmail.com>
2025-04-16 16:19:27 +0700

    Revert "feat: init API"
    This reverts commit 74d7aa5d51e86c343f5f82c30fd01c44240df47b.

--- cmd/main.go
+++ cmd/main.go
@@ -13,7 +13,6 @@ import (
 	"github.com/CP-RektMart/pic-me-pls-backend/internal/jwt"
 	"github.com/CP-RektMart/pic-me-pls-backend/internal/middlewares/authentication"
 	"github.com/CP-RektMart/pic-me-pls-backend/internal/server"
-	"github.com/CP-RektMart/pic-me-pls-backend/internal/services/admin"
 	"github.com/CP-RektMart/pic-me-pls-backend/internal/services/auth"
 	"github.com/CP-RektMart/pic-me-pls-backend/internal/services/category"
 	"github.com/CP-RektMart/pic-me-pls-backend/internal/services/citizencard"
@@ -75,7 +74,6 @@ func main() {
 	customerHandler := customer.NewHandler(store, validate)
 	messageHandler := message.NewHandler(store, authMiddleware, chatService)
 	stripeHandler := stripe.NewHandler(store, validate, authMiddleware, config.Stripe, config.FrontendURL)
-	adminHandler := admin.NewHandler(store, validate, authMiddleware)
 
 	server.RegisterDocs()
 
@@ -95,7 +93,6 @@ func main() {
 		customerHandler,
 		messageHandler,
 		stripeHandler,
-		adminHandler,
 	)
 
 	server.Start(ctx, stop)
