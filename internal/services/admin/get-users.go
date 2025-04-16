6a7f24cd3fddff4cda3fb768f6a2d915f8c593ee
K.Chanatipz <chanatipzaza2003@gmail.com>
2025-04-16 15:30:57 +0700

    feat: get all users api for admin

--- internal/services/admin/get-users.go
+++ internal/services/admin/get-users.go
@@ -1,11 +1,26 @@
 package admin
 
 import (
+	"github.com/CP-RektMart/pic-me-pls-backend/internal/dto"
+	"github.com/CP-RektMart/pic-me-pls-backend/internal/model"
+	"github.com/CP-RektMart/pic-me-pls-backend/pkg/apperror"
 	"github.com/gofiber/fiber/v2"
+	"github.com/pkg/errors"
+	"gorm.io/gorm"
 )
 
 func (h *Handler) HandleGetAllUsers(c *fiber.Ctx) error {
-	return c.Status(200).JSON(fiber.Map{
-		"message": "ok1",
+	var users []model.User
+
+	if err := h.store.DB.Find(&users).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return apperror.NotFound("users not found", err)
+		}
+
+		return errors.Wrap(err, "failed fetch users")
+	}
+
+	return c.Status(fiber.StatusOK).JSON(dto.HttpResponse[[]dto.PublicUserResponse]{
+		Result: dto.ToPublicUserResponses(users),
 	})
 }
