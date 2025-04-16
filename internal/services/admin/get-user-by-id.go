682f3f9305e4f84c2de7e4588439ccdbbbb9a3c8
K.Chanatipz <chanatipzaza2003@gmail.com>
2025-04-16 15:43:26 +0700

    feat: get user api for admin

--- internal/services/admin/get-user-by-id.go
+++ internal/services/admin/get-user-by-id.go
@@ -1,11 +1,40 @@
 package admin
 
 import (
+	"github.com/CP-RektMart/pic-me-pls-backend/internal/dto"
+	"github.com/CP-RektMart/pic-me-pls-backend/internal/model"
+	"github.com/CP-RektMart/pic-me-pls-backend/pkg/apperror"
 	"github.com/gofiber/fiber/v2"
+	"github.com/pkg/errors"
+	"gorm.io/gorm"
 )
 
 func (h *Handler) HandleGetUserByID(c *fiber.Ctx) error {
-	return c.Status(200).JSON(fiber.Map{
-		"message": "ok2",
+	var req dto.GetPhotographerByIDRequest
+	if err := c.ParamsParser(&req); err != nil {
+		return apperror.BadRequest("invalid path params", err)
+	}
+	if err := h.validate.Struct(req); err != nil {
+		return apperror.BadRequest("invalid path params", err)
+	}
+
+	user, err := h.getUserByID(req.ID)
+	if err != nil {
+		return errors.Wrap(err, "failed get user by id")
+	}
+
+	return c.Status(fiber.StatusOK).JSON(dto.HttpResponse[dto.PublicUserResponse]{
+		Result: dto.ToPublicUserResponse(user),
 	})
+}
+
+func (h *Handler) getUserByID(ID uint) (model.User, error) {
+	var user model.User
+	if err := h.store.DB.First(&user, ID).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return user, apperror.NotFound("User not found", err)
+		}
+		return user, errors.Wrap(err, "failed fetch user")
+	}
+	return user, nil
 }
