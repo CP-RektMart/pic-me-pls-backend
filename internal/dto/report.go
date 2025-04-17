a8b1680c7b6b23a83985caaebdb34145e80d15d7
Phumsirii <phumsirisumativit@gmail.com>
2025-04-17 21:46:29 +0700

    fix: remove reportid from edible field

--- internal/dto/report.go
+++ internal/dto/report.go
@@ -1,7 +1,5 @@
 package dto
 
-import "github.com/CP-RektMart/pic-me-pls-backend/internal/model"
-
 type CreateReportRequest struct {
 	QuotationID uint   `json:"quotationId" validate:"required"`
 	Message     string `json:"message" validate:"required"`
@@ -9,8 +7,7 @@ type CreateReportRequest struct {
 }
 
 type UpdateReportRequest struct {
-	ReportID uint               `params:"id" validate:"required"`
-	Message  string             `json:"message"`
-	Title    string             `json:"title"`
-	Status   model.ReportStatus `json:"status"`
+	ReportID uint   `params:"id" validate:"required"`
+	Message  string `json:"message"`
+	Title    string `json:"title"`
 }
